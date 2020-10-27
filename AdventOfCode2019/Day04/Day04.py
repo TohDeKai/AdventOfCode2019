2446f43c06e1066594f8fedbad56c0fe34b3b439
TohDeKai <tohdekai@outlook.com>
2020-10-27 23:19:21 +0800

    Completed Part 1 of Day04

--- AdventOfCode2019/Day04/Day04.py
+++ AdventOfCode2019/Day04/Day04.py
@@ -4,22 +4,19 @@ def possible_password(number=int):
     # It is a six-digit number.
     if len(number) != 6:
         return False
+    #Going from left to right, the digits never decrease; 
+    #they only ever increase or stay the same (like 111123 or 135679).
     else:
-        for i in range(len(number) - 1):
-            #Going from left to right, the digits never decrease; 
-            #they only ever increase or stay the same (like 111123 or 135679).
-            if number[i] > number[i + 1]:
-                return False
-            else:
-                if number[-1] < number[-2]:
-                    return False
+        if list(str(number)) != sorted(list(str(number))):
+            return False
+        else:
+            for i in range(len(number) - 1):
+                currentnumber = number[i]
+                #Two adjacent digits are the same (like 22 in 122345).
+                if number[i] == number[i + 1]:
+                    return True
                 else:
-                    currentnumber = number[i]
-                    #Two adjacent digits are the same (like 22 in 122345).
-                    if number[i] == number[i + 1]:
-                        return True
-                    else:
-                        continue
+                    continue
     return False
 
 '''
@@ -30,8 +27,10 @@ print (possible_password(test_case1))
 print (possible_password(test_case2))
 print (possible_password(test_case3))
 '''
-answer = 0
+
+part1answer = 0
 for i in range(138241,674034):
    if possible_password(i) == True:
-       answer += 1
-print (answer)
+       part1answer += 1
+print (part1answer)
+
